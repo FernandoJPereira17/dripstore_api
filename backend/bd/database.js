2b25a61e0f529360bed9e338724144cc933ebc3a
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2024-05-13 18:34:32 -0300

    Config connection

--- backend/bd/database.js
+++ backend/bd/database.js
@@ -1,13 +1,16 @@
 import { Sequelize } from "sequelize";
 import 'dotenv/config.js';
 
+//console.log('process', process.env.PGHOST, process.env.PGDATABASE);
+
+const HOST = process.env.PGHOST
+const DATABASE = process.env.PGDATABASE
+const USER = process.env.PGUSER
+const PASS = process.env.PGPASSWORD
+
 const STRING_CONNECTION = `postgresql://${USER}:${PASS}@${HOST}/${DATABASE}?sslmode=require`;
 
-const HOST = process.env.PGHOST;
-const DATABASE = process.env.PGDATABASE;
-const USER = process.env.PGUSER;
-const PASS = process.env.PGPASSOWORD;
+console.log(`[STRING_CONNECTION]: ${STRING_CONNECTION}`);
 
+export const sequelize = new Sequelize(STRING_CONNECTION);
 
-
-export const sequelize = new Sequelize ();
