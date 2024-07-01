df66d9d0164c821ff82ffc3e7d0bc122c32cf255
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2024-07-01 20:44:18 -0300

    Routes com Postman Teste

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ import { authService } from '../backend/services/auth.service.js';
 var routerAuth = express.Router()
 
 routerAuth
+
     .post ('/signup', authService.signup)
     .post ('/signin', authService.signin)
     .post('logout', authService.logout)
